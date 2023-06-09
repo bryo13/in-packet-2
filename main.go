96e934a464192dda32bb3959c45f36c1a6b25ff6
Brian Tum <brian.tum10@yahoo.com>
2023-06-09 13:35:17 +0300

    Added more functionality to https://github.com/bryo13/in-packets/tree/master'

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"inPacket/logs"
+	"inPacket/privateIP"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	lgs := new(logs.WriteLogs)
+	pIP := new(privateIP.PrivateIP)
+
+	lgs.WriteIntoLogFile(errors.New("Logs for " + time.Now().String()))
+	interfaces := pIP.CheckInterface()
+	pIP.ReadIpandCheck(interfaces)
+
+	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatal("filename not specified")
+	}
+	filename = flag.Args()[0]
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", serveWs)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal(err)
+	}
+}
